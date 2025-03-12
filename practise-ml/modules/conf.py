e8b61871fe585bcfc2ce01e028493892022544c4
harryng0312 <little.ruan@gmail.com>
2025-03-13 00:18:43 +0700

    init

---
+++ practise-ml/modules/conf.py
@@ -0,0 +1,5 @@
+import os
+
+MODULES_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ""))
+PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), ''))
+
